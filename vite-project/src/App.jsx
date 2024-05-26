d791494d9c53632fa311729dd391101ad2f5b1c8
Gravit09 <garvitchoudhary09@gmail.com>
2024-05-27 00:55:38 +0530

    ch

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./Components/Navbar/Navbar";
+import "./App.scss";
+import Gigs from "./Pages/gigs/Gigs";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Gigs />
+    </div>
+  );
+}
+
+export default App;
---
+++ vite-project/src/Pages/gigs/Gigs.jsx
@@ -0,0 +1,65 @@
+import React, { useRef, useState } from "react";
+import "./Gigs.scss";
+import { gigs } from "../../Data.js";
+import GigCard from "../../Components/gigCard/GigCard";
+
+function Gigs() {
+  const [sort, setSort] = useState("sales");
+  const [open, setOpen] = useState(false);
+  const minRef = useRef();
+  const maxRef = useRef();
+
+  const reSort = (type) => {
+    setSort(type);
+    setOpen(false);
+  };
+
+  const apply = () => {
+    console.log(minRef.current.value);
+    console.log(maxRef.current.value);
+  };
+
+  return (
+    <div className="gigs-component">
+      <div className="gigs-container">
+        <span className="gigs-breadcrumbs">Liverr - Graphics & Design -</span>
+        <h1>AI Artists</h1>
+        <p>
+          Explore the boundaries of art and technology with Liverr's AI artists
+        </p>
+        <div className="gigs-menu">
+          <div className="gigs-left">
+            <span>Budget</span>
+            <input ref={minRef} type="number" placeholder="min" />
+            <input ref={maxRef} type="number" placeholder="max" />
+            <button onClick={apply}>Apply</button>
+          </div>
+          <div className="gigs-right">
+            <span className="gigs-sortBy">Sort by</span>
+            <span className="gigs-sortType">
+              {sort === "sales" ? "Best Selling" : "Newest"}
+            </span>
+            <img src="./img/down.png" alt="" onClick={() => setOpen(!open)} />
+            {open && (
+              <div className="gigs-rightMenu">
+                {sort === "sales" ? (
+                  <span onClick={() => reSort("createdAt")}>Newest</span>
+                ) : (
+                  <span onClick={() => reSort("sales")}>Best Selling</span>
+                )}
+                <span onClick={() => reSort("sales")}>Popular</span>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="gigs-cards">
+          {gigs.map((gig) => (
+            <GigCard key={gig.id} item={gig} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Gigs;
